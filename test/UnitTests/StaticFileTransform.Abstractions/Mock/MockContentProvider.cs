82d009c26becefbc5ba10af3140b91617ee8349a
Eke Péter <peterekepeter@gmail.com>
2018-06-08 19:18:18 +0300

    Added unit test project.

---
+++ test/UnitTests/StaticFileTransform.Abstractions/Mock/MockContentProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StaticFileTransform.Abstractions;
+
+namespace UnitTests.StaticFileTransform.Abstractions.Mock
+{
+    public class MockContentProvider : IContentProvider
+    {
+        public Dictionary<String, String> Content;
+
+        public MockContentProvider() => Content = new Dictionary<string, string>();
+
+        public MockContentProvider(Dictionary<string, string> store) => Content = store;
+
+        public string GetContent(string filename) => Content.GetValueOrDefault(filename);
+    }
+}
